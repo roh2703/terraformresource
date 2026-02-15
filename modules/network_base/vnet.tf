df9f16b4c6be550af35477b0e1bf6190fc54936c
Rohit Kudumulla <138331321+roh2703@users.noreply.github.com>
2026-02-14 23:12:04 -0600

    Rename virtual network resource and update properties

--- modules/network_base/vnet.tf
+++ modules/network_base/vnet.tf
@@ -1,7 +1,7 @@
-resource "azurerm_virtual_network" "this" {
+resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   address_space = var.vnet_address_space
   dns_servers   = var.dns_servers
