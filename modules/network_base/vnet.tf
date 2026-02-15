353e02c8f0e3f3e36eac796ab658b50d1a9da2ce
Rohit Kudumulla <138331321+roh2703@users.noreply.github.com>
2026-02-14 23:07:26 -0600

    Update resource group reference in vnet.tf

--- modules/network_base/vnet.tf
+++ modules/network_base/vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "this" {
   name                = var.vnet_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
 
   address_space = var.vnet_address_space
   dns_servers   = var.dns_servers
