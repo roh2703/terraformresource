cfb9ad08ee078830d37485807c0859d7cf0d28be
Rohit Kudumulla <138331321+roh2703@users.noreply.github.com>
2026-02-14 22:40:55 -0600

    Rename azurerm_virtual_network resource to 'vnet'

--- modules/network_base/vnet.tf
+++ modules/network_base/vnet.tf
@@ -1,4 +1,4 @@
-resource "azurerm_virtual_network" "this" {
+resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
